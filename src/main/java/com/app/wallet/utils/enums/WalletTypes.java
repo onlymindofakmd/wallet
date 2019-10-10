e41d6eb9a16ae52e3e1fd1477ebc746f2b0f4386
onlymindofakmd <mindofakmd@gmail.com>
2019-10-10 09:58:16 +0800

    normal update

--- src/main/java/com/app/wallet/utils/enums/WalletTypes.java
+++ src/main/java/com/app/wallet/utils/enums/WalletTypes.java
@@ -1,22 +1,9 @@
 package com.app.wallet.utils.enums;
 
 public enum WalletTypes {
-    TRANSFER(0, "存款"),
-    COMMON(1, "通用"),
-    TRAVEL(2, "旅游"),
-    CHILDREN(3, "孩子"),
-    RESTAURANT(4, "餐馆"),
-    DRINK(5, "饮料"),
-    SNACKS(6, "零食"),
-    CLOTHES(7, "衣服"),
-    HOUSE(8, "住房"),
-    TICKET(9, "车票"),
-    GIFT(10, "礼物"),
-    SURPRISE(12, "惊喜"),
-    BORROW(13, "借入"),
-    BORROW_B(14, "还款"),
-    LEND(15, "借出"),
-    LEND_B(16, "已还");
+    FOOD(0, "食物"),
+    TRAFFIC(1, "交通"),
+    ENTERTAINMENT(2, "娱乐");
 
     private Integer id;
     private String dTypeName;
